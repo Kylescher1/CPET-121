bd9ccb3fef719c67a95bdddc1e1abf9fce2ad7de
KylescherDeskTop <kms3218@rit.edu>
2022-10-29 20:12:38 -0400

    v.2

--- Design-Projects/connectFour.cpp
+++ Design-Projects/connectFour.cpp
@@ -6,7 +6,7 @@ bool userInput(int userTurn, int board[][7], int *R, int *C){
     int userClm;
     std::cin >> userClm;
     
-    if(userClm > 0 && userClm < 7){
+    if(userClm > 0 && userClm <= 7){
         for(int i = 0; i < 7; i++){
             if(board[i][userClm - 1] == 0 ){
                 *R = i;
@@ -16,7 +16,7 @@ bool userInput(int userTurn, int board[][7], int *R, int *C){
         }
     }
     else{
-         return false;
+         return (false);
     }
   
 
@@ -46,46 +46,51 @@ int main(){
     
     do {
         if(userTurn == 1){
-            bool validMove = userInput(userTurn, board, &R, &C);
-            if(validMove = true){
+            validMove = userInput(userTurn, board, &R, &C);
+            if(validMove == true){
                 board[R][C] = 1;
+                userTurn = 2;
             }
             else{
-                std::cout << "Invalid Move";
+                std::cout << "Invalid Move" << std::endl;
             }
-            userTurn = 2;
         }
         else if(userTurn == 2){
-            bool validMove = userInput(userTurn, board, &R, &C);
-            if(validMove = true){
+            validMove = userInput(userTurn, board, &R, &C);
+            if(validMove == true){
                 board[R][C] = 2;
+                userTurn = 1;
             }
             else{
-                std::cout << "Invalid Move";
+                std::cout << "Invalid Move" << std::endl;
             }
-            userTurn = 1;
 
         }
 
 
-
-        std::cout << "  1   2   3   4   5   6   7 " << std::endl;
-        std::cout <<"-----------------------------" << std::endl;
-        for(int i = 5; i >= 0; i--){
-            for(int j = 0; j < 7; j ++){
-                std::cout <<"| "; 
-                if(board[i][j] == 1){
-                    std::cout << "X";
-                }
-                else if (board[i][j] == 2){
-                    std::cout << "O";
-                }
-                else {
+        if(validMove){
+            std::cout << "  1   2   3   4   5   6   7 " << std::endl;
+            std::cout <<"-----------------------------" << std::endl;
+            for(int i = 5; i >= 0; i--){
+                for(int j = 0; j < 7; j ++){
+                    std::cout <<"| "; 
+                    if(board[i][j] == 1){
+                        std::cout << "X";
+                    }
+                    else if (board[i][j] == 2){
+                        std::cout << "O";
+                    }
+                    else {
+                        std::cout << " ";
+                    }
                     std::cout << " ";
                 }
-                std::cout << " ";
+                std::cout << "|" << std::endl;
             }
-            std::cout << "|" << std::endl;
+
+        }
+        else{
+            continue;
         }
 
         winCheck = winFunc(board, winCheck);
