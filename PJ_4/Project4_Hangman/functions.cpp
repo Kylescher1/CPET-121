4d520413da17e3b402ac7cc3ab0d439dcf368fde
Kylescher1 <kms3218@rit.edu>
2022-11-24 13:41:56 -0500

    Hangman proj

--- PJ_4/Project4_Hangman/functions.cpp
+++ PJ_4/Project4_Hangman/functions.cpp
@@ -1,58 +1,87 @@
-#include "hangPerson.h"
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <cstdlib>
 #include <string>
+#include <stdio.h>
+#include <ctype.h>
+#include "hangPerson.h"
 
-void diplay(int input) {
-    switch (input) {
-    case '0':
-        std::cout << "+\n|\n|\n|\n|\n|\n|\n|";
-        std::cout << "+--------\n";
+void display(int input, char guess, std::string word, std::vector<char> &vectWord, std::vector<char> &vectDisplay) {
+
+    std::cout << "\n\n\n";
+
+    switch (input) {//Switch case based on what state the gallows are at
+    case 0:
+        std::cout << "+\n|\n|\n|\n|\n|\n|\n";
+        std::cout << "+ - - - - - - - -\n";
         break;
-    case '1':
-        std::cout << "+-----+\n|\n|\n|\n|\n|\n|\n|";
-        std::cout << "+--------\n";
+    case 1:
+        std::cout << "+-----+\n|\n|\n|\n|\n|\n|\n";
+        std::cout << "+ - - - - - - - -\n";
         break;
-    case '2':
-        std::cout << "+-----+\n|     |\n|\n|\n|\n|\n|\n|";
-        std::cout << "+--------\n";
+    case 2:
+        std::cout << "+-----+\n|     |\n|\n|\n|\n|\n|\n";
+        std::cout << "+ - - - - - - - -\n";
         break;
-    case '3':
-        std::cout << "+-----+\n|     |\n|     O\n|\n|\n|\n|\n|";
-        std::cout << "+--------\n";
+    case 3:
+        std::cout << "+-----+\n|     |\n|     O\n|\n|\n|\n|\n";
+        std::cout << "+ - - - - - - - -\n";
         break;
-    case '4':
-        std::cout << "+-----+\n|     |\n|     O\n|     |\n|     |\n|\n|\n|";
-        std::cout << "+--------\n";
+    case 4:
+        std::cout << "+-----+\n|     |\n|     O\n|     |\n|     |\n|\n|\n";
+        std::cout << "+ - - - - - - - -\n";
         break;
-    case '5':
-        std::cout << "+-----+\n|     |\n|     O\n|     |\n|     |\n|\n|\n|";
-        std::cout << "+--------\n";
+    case 5:
+        std::cout << "+-----+\n|     |\n|     O\n|    \\|\n|     |\n|\n|\n";
+        std::cout << "+ - - - - - - - -\n";
         break;
-    case '6':
-        std::cout << "+-----+\n|     |\n|     O\n|     |/\n|     |\n|\n|\n|";
-        std::cout << "+--------\n";
+    case 6:
+        std::cout << "+-----+\n|     |\n|     O\n|    \\|/\n|     |\n|\n|\n";
+        std::cout << "+ - - - - - - - -\n";
         break;
-    case '7':
-        std::cout << "+-----+\n|     |\n|     O\n|     |/\n|     /|n|\n|\n|";
-        std::cout << "+--------\n";
+    case 7:
+        std::cout << "+-----+\n|     |\n|     O\n|    \\|/\n|     |\n|    / \n|\n";
+        std::cout << "+ - - - - - - - -\n";
         break;
-    case '8':
-        std::cout << "+-----+\n|     |\n|     O\n|     |/\n|     /|\n|\n|\n|";
-        std::cout << "+--------\n";
+    case 8:
+        std::cout << "+-----+\n|     |\n|     O\n|    \\|/\n|     |\n|    / \\\n|\n";
+        std::cout << "+ - - - - - - - -\n";
         break;
 
     }
+
+    std::cout << "\n\n";
+    
+    int wordLength;
+    wordLength = word.length();
+
+
+    for (int i = 0; i < wordLength; i++) {
+        if (vectWord.at(i) == guess) {
+            vectDisplay.at(i) = guess;
+        }
+    }
+
+    
+
+    for (int i = 0; i < wordLength; i++) {//todo char insert
+        std::cout << vectDisplay.at(i) << " ";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < wordLength; i++) {
+        std::cout << "_ ";
+
+    }
+
 }
 
 
-std::string selectWord(std::string &word) {
+
+std::string selectWord(std::string word) {
     std::ifstream inFile;
     std::string filename;
     int total_lines = 0;
-    int randNum = rand() % total_lines;
     filename = "words_1.txt";
     std::string line;
     std::vector<std::string> lines;
@@ -76,9 +105,10 @@ std::string selectWord(std::string &word) {
 
     inFile.close();
 
-    std::cout << (lines[randNum]);
+    int randNum = rand() % total_lines;
+    word = (lines[randNum]);
 
-    return 0 ;
+    return word ;
 }
 
 //gameInput
@@ -87,6 +117,33 @@ only takes lowercase input
 passes to the main function
 verifes input
 */
+char gameInput(char &guess, int instruction, std::vector <char>& vectInputs) {
+    bool valid = false;
+    std::cout << "\n\nPlease enter your guess :";
+
+    if (instruction == 1) {//todo remove 
+        while (!valid) {
+            start:
+            std::cin >> guess;
+            for (int i = 0; i < vectInputs.size(); i++) {
+                if (vectInputs.at(i) == guess) {
+                    std::cout << "You have already guessed this letter!\nPlease try again :";
+                    goto start;
+                }
+            }
+            if (isalpha(guess)) {//checks for lowecase letter
+                vectInputs.push_back(guess);
+                valid = true;
+            }
+            else {
+                std::cout << "This is a letter!\nPlease try again :";
+                continue;
+            }
+        }
+    }
+    return guess;
+
+}
 
 
 //recordResults
--- PJ_4/Project4_Hangman/main.cpp
+++ PJ_4/Project4_Hangman/main.cpp
@@ -1,26 +1,79 @@
-#include "hangPerson.h"
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <cstdlib>
 #include <string>
+#include <cstring>
+#include "hangPerson.h"
 
 int main() {
 
-    std::string word = "none";
+    std::string word = "ZZZZZZZZ";
+    bool valid = false;
+    bool playAgain = true;
+    std::string play;
+    char guess = ' ';
     int turn = 0;
 
-    //gamestart
-    std::cout << "Welcome to hangperson!" << std::endl;
+    std::cout << "Welcome to hangperson!\nRandomly picking a word..." << std::endl;
 
-    word = selectWord(word);
-    std::cout << word;
+    while (playAgain) {
+        word = selectWord(word);//Random word selection
+        int wordLength = word.length();
+        std::vector<char> vectWord(word.begin(), word.end());
+        std::vector<char> vectInputs;
 
-    display(turn);
+        std::vector<char> vectDisplay;
+        for (int i = 0; i < wordLength; i++) {//Fill the display vector with astericks. Needs to be done in main
+            vectDisplay.push_back('*');
+        }
 
+        display(turn, guess, word, vectWord, vectDisplay);
 
-    //call game input
-    //gameInput(word);
+        while (!valid) {
+            int count = 0;
+            int goodMove = false;
 
-    //call display with 
+            gameInput(guess, 1, vectInputs);
+            for (int i = 0; i < wordLength; i++) {
+                if (guess == vectWord.at(i)) {
+                    goodMove = true;
+                }
+            }
+            if (!goodMove) {
+                turn++;
+                std::cout <<"\n\n" << guess << " is not in this word!";
+            }
+            display(turn, guess, word, vectWord, vectDisplay);
+            for (int i = 0; i < wordLength; i++) {
+                if (vectDisplay.at(i) == vectWord.at(i)) {
+                    count++;
+                }
+                if (count == wordLength) {
+                    std::cout << "\n\n\nYou win!\nYour word was " << word;
+                    valid = true;
+                    break;
+                }
+            }
+            if (turn == 8) {
+                std::cout << "\n\n\nYou loose!\nThe word was " << word;
+                valid = true;
+                break;
+            }
+        }
+        turn = 0;
+        //Record Results 
+
+        std::cout << "\n\n\nWould you like to play again? [Y/N] :";
+        std::cin >> play;
+        if (play == "Y" || play == "y") {
+            playAgain = true;
+            valid = false;
+            std::cout << "Clearing the gallows...\nChoosing new word...";
+        }
+        else {
+            playAgain = false;
+            std::cout << "Sorry to see you go, quitting program...";
+        }
+    }
 }
