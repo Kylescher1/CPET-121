eb6f65025f7a98959f14d6ec7e040e9e55043ebf
Kylescher1 <kms3218@rit.edu>
2022-11-25 14:09:53 -0500

    structure changes

--- Project4_Hangman/functions.cpp
+++ Project4_Hangman/functions.cpp
@@ -1,34 +1,177 @@
-#include "hangPerson.h"
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <string>
+#include <stdio.h>
+#include <ctype.h>
+#include <cctype>
+#include "hangPerson.h"
 
-//display
-/*
-takes the input from 0-8
-*/
-void diplay(int input){
+void display(int input, char guess, std::string word, std::vector<char> &vectWord, std::vector<char> &vectDisplay) {
+    std::cout << "\n\n\n";
+    switch (input) {//Switch case based on what state the gallows are at
+    case 0:
+        std::cout << "+\n|\n|\n|\n|\n|\n|\n";
+        std::cout << "+ - - - - - - - -\n";
+        break;
+    case 1:
+        std::cout << "+-----+\n|\n|\n|\n|\n|\n|\n";
+        std::cout << "+ - - - - - - - -\n";
+        break;
+    case 2:
+        std::cout << "+-----+\n|     |\n|\n|\n|\n|\n|\n";
+        std::cout << "+ - - - - - - - -\n";
+        break;
+    case 3:
+        std::cout << "+-----+\n|     |\n|     O\n|\n|\n|\n|\n";
+        std::cout << "+ - - - - - - - -\n";
+        break;
+    case 4:
+        std::cout << "+-----+\n|     |\n|     O\n|     |\n|     |\n|\n|\n";
+        std::cout << "+ - - - - - - - -\n";
+        break;
+    case 5:
+        std::cout << "+-----+\n|     |\n|     O\n|    \\|\n|     |\n|\n|\n";
+        std::cout << "+ - - - - - - - -\n";
+        break;
+    case 6:
+        std::cout << "+-----+\n|     |\n|     O\n|    \\|/\n|     |\n|\n|\n";
+        std::cout << "+ - - - - - - - -\n";
+        break;
+    case 7:
+        std::cout << "+-----+\n|     |\n|     O\n|    \\|/\n|     |\n|    / \n|\n";
+        std::cout << "+ - - - - - - - -\n";
+        break;
+    case 8:
+        std::cout << "+-----+\n|     |\n|     O\n|    \\|/\n|     |\n|    / \\\n|\n";
+        std::cout << "+ - - - - - - - -\n";
+        break;
+    }
+    std::cout << "\n\n";
+    
+    int wordLength;
+    wordLength = word.length();
 
+    for (int i = 0; i < wordLength; i++) {
+        if (vectWord.at(i) == guess) {
+            vectDisplay.at(i) = guess;
+        }
+    }
+    for (int i = 0; i < wordLength; i++) {//todo char insert
+        std::cout << vectDisplay.at(i) << " ";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < wordLength; i++) {
+        std::cout << "_ ";
+    }
 }
 
-//selectWord
-/*
-randomizes word selection
-*/
+std::string selectWord(std::string word) {//Random word selection 
+    std::ifstream inFile;
+    std::string filename;
+    int total_lines = 0;
+    filename = "words_1.txt";
+    std::string line;
+    std::vector<std::string> lines;
+    inFile.open(filename.c_str());
 
-std::string randomWord(std::string word){
+    if (!inFile.is_open()) {
+        std::cout << "\n Could not open file ... program terminated \n";
+    }
+    srand(time(NULL));
+   
+    while (std::getline(inFile, line)){
+        total_lines++;
+        lines.push_back(line);
+    }
+    inFile.close();
 
-    return(word);
+    int randNum = rand() % total_lines;
+    word = (lines[randNum]);
+    return word;
 }
 
-//gameInput
-/*
-only takes lowercase input
-passes to the main function
-verifes input
-*/
+char gameInput(char &guess, int instruction, std::vector <char>& vectInputs) {
+    bool valid = false;
+    std::cout << "\n\nPlease enter your guess :";
+
+    if (instruction == 1) {//todo remove 
+        while (!valid) {
+            start:
+            std::cin >> guess;
+            for (int i = 0; i < vectInputs.size(); i++) {
+                if (vectInputs.at(i) == guess) {
+                    std::cout << "You have already guessed this letter!\nPlease try again :";
+                    goto start;
+                }
+            }
+            if (isalpha(guess) && islower(guess)) {//checks for lowecase letter
+                vectInputs.push_back(guess);
+                valid = true;
+            }
+            else {
+                std::cout << "This is not a valid input!\nPlease try again :";
+                continue;
+            }
+        }
+    }
+    return guess;
+}
 
 
-//recordResults
-/*
-saves results
-*/
+void recordResults(std::string word, char guess, bool valid, bool goodMove, std::vector <char> vectInputs, int turn) {
+    std::ofstream outFile;
+    std::string filename = "results_1.txt";
+    int wordLength = wordLength = word.length();
+    std::vector<char> vectWord(word.begin(), word.end());
+    turn = 0;
 
+    outFile.open(filename.c_str());
+
+    if (!outFile.is_open()) {
+        std::cout << "Output file error!";
+    }
+
+    outFile << "The word is " << word;
+    outFile << "\n";
+
+    for (int i = 0; i < vectInputs.size(); i++) {
+        outFile << "\nThe user has guessed " << vectInputs.at(i) << " which was ";
+
+        for (int j = 0; j < wordLength; j++) {
+            if (vectInputs.at(i) == vectWord.at(j)) {
+                goodMove = true;
+                break;
+            }
+            else {
+                goodMove = false;
+            }
+        }
+
+        if (!goodMove) {
+            turn++;
+        }
+
+        if (!goodMove) {//Output if the letter guessed by a user is not in the word
+            outFile << " not found in this word!";
+            outFile << " The error count is :" << turn;
+
+        }
+        else {
+            outFile << "found in the word!";
+        }
+        outFile << std::endl;
+    }
+  
+
+    if (valid) {//Records if user found word
+        outFile << "\nThe user found the word!";
+    }
+    else {
+        outFile << "\nThe user failed to find the word and has been hung...";
+    }
+    outFile.close();
+}
+
+//test
--- Project4_Hangman/hangPerson.h
+++ Project4_Hangman/hangPerson.h
@@ -1,8 +1,10 @@
 #ifndef HANGPERSON_H_INCLUDED
 #define HANGPERSON_H_INCLUDED
+#include <string>
 
-int display(int input);
-std::string randomWord(std::string word);
+void recordResults(std::string word, char guess, bool valid, bool goodMove, std::vector <char> vectInputs, int turn);
+void display(int input, char guess, std::string word, std::vector<char> &vectWord, std::vector<char> &vectDisplay);
+char gameInput(char& guess, int instruction, std::vector<char> &vectInputs);
+std::string selectWord(std::string word);
 
-
-#endif // !HANGPERSON_H_INCLUDED
+#endif // !TEST
